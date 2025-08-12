0416fe6ac788a42d1d0d3c75342a20a7caff3967
슬기 <jsg8369@naver.com>
2025-08-12 19:00:47 +0900

    De2

---
+++ Assets/ScriptableObject/ItemData.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+
+public enum ItemType
+{
+    Equipable,
+    Consumable,
+    Resource
+}
+
+public enum ConsumableType
+{
+    Health,
+    Hunger
+}
+
+[Serializable]
+public class ItemDataConsumbale
+{
+    public ConsumableType Type;
+    public float value;
+}
+
+[CreateAssetMenu(fileName = "Item", menuName = "New Item")]
+public class ItemData : ScriptableObject
+{
+    [Header("Info")]
+    public string displayName;
+    public string description;
+    public ItemType type;
+    public Sprite icon;
+    public GameObject dropPrefab;
+
+    [Header("Stacking")] 
+    public bool canStack;
+    public int maxStackAmont;
+    
+    [Header("Consumable")]
+    public ItemDataConsumbale[] consumbales;
+}
